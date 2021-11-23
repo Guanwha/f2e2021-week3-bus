e23be137213b849b29ee4fe69f9a22a1fa854afd
Eric Fei <guanwha@gmail.com>
2021-11-24 01:16:28 +0800

    Initialized tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./public/**/*.html', './src/**/*.vue'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1200px',
+      // xl: '1280px',
+      // '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        'main-500': '#1CC8EE',
+        'dark-900': '#131414',
+        'dark-800': '#1C1D1D',
+        'dark-700': '#414242',
+        'light-800': '#F5F5F5',
+      },
+      fill: {
+        'main-500': '#1CC8EE',
+        'dark-900': '#131414',
+        'dark-800': '#1C1D1D',
+        'dark-700': '#414242',
+        'light-800': '#F5F5F5',
+      },
+      zIndex: {
+        'top': 99999,
+      },
+    },
   },
   variants: {
     extend: {},
