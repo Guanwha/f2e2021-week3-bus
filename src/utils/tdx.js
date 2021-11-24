68df58a24da19287fb2f109c7fafeecf39565a99
Eric Fei <guanwha@gmail.com>
2021-11-24 23:42:56 +0800

    Installed axios & added 相關的 utils
    1. add some utilities
        - net.js
        - message.js
        - tdx.js

---
+++ src/utils/tdx.js
@@ -0,0 +1,15 @@
+import JsSHA from 'jssha';
+
+const getTDXHeader = () => {
+  const AppID = process.env.VUE_APP_TDXID;
+  const AppKey = process.env.VUE_APP_TDXKEY;
+  const GMTString = new Date().toGMTString();
+  const ShaObj = new JsSHA('SHA-1', 'TEXT');
+  ShaObj.setHMACKey(AppKey, 'TEXT');
+  ShaObj.update(`x-date: ${GMTString}`);
+  const HMAC = ShaObj.getHMAC('B64');
+  const Authorization = `hmac username="${AppID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
+  return { Authorization, 'X-Date': GMTString };
+};
+
+export default getTDXHeader;
